38d974b2edce1ef7320ed853181f3a9cfab78f9f
Nick Fox-Gieg <nick@fox-gieg.com>
2018-10-31 16:24:43 -0400

    change dimensions

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -5,10 +5,12 @@ void ofApp::setup(){
     
     ofSetVerticalSync(false);
     _realsense2.setupDevice(0);
-    _realsense2.setupColor(640, 360, 30);
-    _realsense2.setupIR(640, 360, 30);
-    _realsense2.setupDepth(640, 360, 30);
+    _realsense2.setupColor(848, 480, 30);
+    //_realsense2.setupIR(640, 360, 30);
+    _realsense2.setupDepth(848, 480, 30);
     _realsense2.startPipeline(true);
+    
+    syphon.setName("RealSyphon");
     
     _gui.setup("appSettings.xml");
     _gui.add(_realsense2.getGui());
@@ -26,15 +28,17 @@ void ofApp::draw(){
     if(_realsense2.colorEnabled())
         _realsense2.getColorTex()->draw(0, 0);
     
-    if(_realsense2.irEnabled())
-        _realsense2.getIrTex()->draw(640, 0);
+    //if(_realsense2.irEnabled())
+        //_realsense2.getIrTex()->draw(640, 0);
     
     if(_realsense2.depthEnabled()) {
-        _realsense2.getRawDepthTex()->draw(0, 360);
-        _realsense2.getDepthTex()->draw(640, 360);
+        //_realsense2.getRawDepthTex()->draw(0, 360);
+        _realsense2.getDepthTex()->draw(0, 480);
     }
     
     ofDrawBitmapString(ofToString(ofGetFrameRate()), 10, 10);
+    
+    syphon.publishScreen();
     
     _gui.draw();
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,16 +2,21 @@
 
 #include "ofMain.h"
 #include "ofxLibRealSense2.hpp"
+#include "ofxSyphon.h"
 
 class ofApp : public ofBaseApp{
 
-public:
-    void setup();
-    void update();
-    void draw();
-    void exit();
-    void keyPressed(int key);
+    public:
+        void setup();
+        void update();
+        void draw();
+        void exit();
+        void keyPressed(int key);
     
-    ofxLibRealSense2 _realsense2;
-    ofxPanel         _gui;
+        ofxLibRealSense2 _realsense2;
+        ofxPanel         _gui;
+    
+        ofTexture tex;
+        ofxSyphonServer syphon;
+
 };
